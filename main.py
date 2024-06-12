63c038ce03f59b3cb5760fa621bafe7a988a4e51
Gugamaral <guamaral@gmail.com>
2024-06-11 21:19:14 -0300

    Verso 1.0

--- main.py
+++ main.py
@@ -23,5 +23,10 @@ auto.press('enter')
 auto.write('git push -u origin main')
 auto.press('enter')
 
+auto.write('pip install cx_Freeze')
+auto.press('enter')
+
+auto.write('cxfreeze main.py --target-dir pasta-autoguit')
+auto.press('enter')
 
 
